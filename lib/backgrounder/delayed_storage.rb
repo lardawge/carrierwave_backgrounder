654b76eed594ed4aef0f6148201d122923adffe6
Larry Sprock <larry@lucidbleu.com>
2011-03-21 10:19:05 -0700

    Rename method

--- lib/backgrounder/delayed_storage.rb
+++ lib/backgrounder/delayed_storage.rb
@@ -8,12 +8,12 @@ module CarrierWave
       
       included do
         ::ActiveRecord::Base.send :include, ::CarrierWave::Backgrounder::ActiveRecord
-        send :stub_version_caching!
+        send :halt_versioning!
       end
       
       module ClassMethods
  
-        def stub_version_caching!
+        def halt_versioning!
           class_eval <<-RUBY, __FILE__, __LINE__ + 1
             def cache_versions!(new_file)
               super(new_file) if model.process_upload?
