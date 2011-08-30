455f354da4c3a7cf521234cdd97937ba0e274d16
Larry Sprock <larry@lucidbleu.com>
2011-08-29 22:45:21 -0700

    Refactor specs

--- spec/workers/process_asset_spec.rb
+++ spec/workers/process_asset_spec.rb
@@ -2,24 +2,22 @@
 require 'spec_helper'
 require 'backgrounder/workers/process_asset'
 
-describe worker = CarrierWave::Workers::ProcessAsset do
-  before do
-    @user   = mock('User')
-    @image  = mock('UserAsset')
-    @worker = worker.new(@user, '22', :image)
-  end
+describe CarrierWave::Workers::ProcessAsset do
+  let(:user)   { mock('User') }
+  let(:image)  { mock('UserAsset') }
+  let(:worker) { CarrierWave::Workers::ProcessAsset.new(user, '22', :image) }
 
   context "#perform" do
     it 'processes versions' do
-      @user.expects(:find).with('22').returns(@user).once
-      @user.expects(:image).once.returns(@image)
-      @user.expects(:process_image_upload=).with(true).once
+      user.expects(:find).with('22').returns(user).once
+      user.expects(:image).once.returns(image)
+      user.expects(:process_image_upload=).with(true).once
 
-      @image.expects(:recreate_versions!).once.returns(true)
-      @user.expects(:respond_to?).with(:image_processing).once.returns(true)
-      @user.expects(:update_attribute).with(:image_processing, nil).once
+      image.expects(:recreate_versions!).once.returns(true)
+      user.expects(:respond_to?).with(:image_processing).once.returns(true)
+      user.expects(:update_attribute).with(:image_processing, nil).once
 
-      @worker.perform
+      worker.perform
     end
   end
 end
--- spec/workers/store_asset_spec.rb
+++ spec/workers/store_asset_spec.rb
@@ -2,28 +2,25 @@
 require 'spec_helper'
 require 'backgrounder/workers/store_asset'
 
-describe worker = CarrierWave::Workers::StoreAsset do
-  before do
-    @user   = mock('User')
-    @image  = mock('UserAsset')
-    @worker = worker.new(@user, '22', :image)
-  end
+describe CarrierWave::Workers::StoreAsset do
+  let(:user)   { mock('User') }
+  let(:image)  { mock('UserAsset') }
+  let(:worker) { CarrierWave::Workers::StoreAsset.new(user, '22', :image) }
 
   context "#perform" do
     it 'processes versions' do
-      File.expects(:open).with('../fixtures/test.jpg').once.returns('apple')
-      FileUtils.expects(:rm).with('../fixtures/test.jpg').once
-      @user.expects(:find).with('22').once.returns(@user)
-      @user.expects(:image_tmp).once.returns('test.jpg')
-      @user.expects(:image).once.returns(@image)
-      @image.expects(:root).once.returns('..')
-      @image.expects(:cache_dir).once.returns('fixtures')
-      @user.expects(:process_image_upload=).with(true).once
-      @user.expects(:image=).with('apple').once
-      @user.expects(:image_tmp=).with(nil).once
-      @user.expects(:save!).once.returns(true)
+      FileUtils.expects(:rm).with(File.expand_path('../fixtures/test.jpg', __FILE__)).once
+      user.expects(:find).with('22').once.returns(user)
+      user.expects(:image_tmp).once.returns('test.jpg')
+      user.expects(:image).once.returns(image)
+      image.expects(:root).once.returns(File.expand_path '..', __FILE__)
+      image.expects(:cache_dir).once.returns('fixtures')
+      user.expects(:process_image_upload=).with(true).once
+      user.expects(:image=).once
+      user.expects(:image_tmp=).with(nil).once
+      user.expects(:save!).once.returns(true)
 
-      @worker.perform
+      worker.perform
     end
   end
 end
