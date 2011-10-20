f39c9dd2eb4f31a378d2b0a5fb2200c433a21ced
Darren Terhune <darren@headfirstproductions.ca>
2011-10-20 12:24:03 -0700

    fix #7 process all in background

--- lib/backgrounder/workers/store_asset.rb
+++ lib/backgrounder/workers/store_asset.rb
@@ -10,12 +10,12 @@ module CarrierWave
           asset = record.send(:"#{column}")
           cache_dir  = [asset.root, asset.cache_dir].join("/")
           cache_path = [cache_dir, tmp].join("/")
-          tmp_dir = [cache_dir, tmp.split("/")[0]].join("/")
+
           record.send :"process_#{column}_upload=", true
           record.send :"#{column}_tmp=", nil
           File.open(cache_path) { |f| record.send :"#{column}=", f }
           if record.save!
-            FileUtils.rm_r(tmp_dir, :force => true)
+            FileUtils.rm(cache_path)
           end
         end
       end
