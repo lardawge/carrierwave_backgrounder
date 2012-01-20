3e7844923dbefee225bfde1b75e4074b92c0e954
Julian Tescher <virulent@gmail.com>
2012-01-20 14:02:00 -0800

    Add support for carrierwave-mongoid's #recreate_versions! (which sometimes returns nil for remotely hosted files)

--- lib/backgrounder/workers/process_asset.rb
+++ lib/backgrounder/workers/process_asset.rb
@@ -13,7 +13,7 @@ module CarrierWave
         resource = klass.is_a?(String) ? klass.constantize : klass
         record = resource.find id
         record.send(:"process_#{column}_upload=", true)
-        if record.send(:"#{column}").recreate_versions! && record.respond_to?(:"#{column}_processing")
+        if record.send(:"#{column}").to_s && record.send(:"#{column}").recreate_versions! && record.respond_to?(:"#{column}_processing")
           record.update_attribute :"#{column}_processing", nil
         end
       end
