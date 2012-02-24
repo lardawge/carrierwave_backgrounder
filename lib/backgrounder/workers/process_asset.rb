b2433d52f4099a758bd85d563800939405b49132
Adrian Castillo <rodacato@gmail.com>
2012-02-24 13:13:09 -0600

    Prevent ActiveRecord::RecordNotFound errors

--- lib/backgrounder/workers/process_asset.rb
+++ lib/backgrounder/workers/process_asset.rb
@@ -11,14 +11,17 @@ module CarrierWave
 
       def perform
         resource = klass.is_a?(String) ? klass.constantize : klass
-        record = resource.find id
-        record.send(:"process_#{column}_upload=", true)
-        if record.send(:"#{column}").recreate_versions! && record.respond_to?(:"#{column}_processing")
-          record.update_attribute :"#{column}_processing", nil
+        record = resource.find_by_id id
+
+        if record
+          record.send(:"process_#{column}_upload=", true)
+          if record.send(:"#{column}").recreate_versions! && record.respond_to?(:"#{column}_processing")
+            record.update_attribute :"#{column}_processing", nil
+          end
         end
       end
-      
+
     end # ProcessAsset
-    
+
   end # Workers
 end # Backgrounder
