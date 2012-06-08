f980dc677da0de6b035cdf282dab314b414e97b7
Frederico Araujo <fred.the.master@gmail.com>
2012-06-08 19:09:38 +0700

    Sidekiq Support fix #3

--- lib/backgrounder/workers/store_asset.rb
+++ lib/backgrounder/workers/store_asset.rb
@@ -3,29 +3,50 @@ module CarrierWave
   module Workers
 
     class StoreAsset < Struct.new(:klass, :id, :column)
-      @queue = :store_asset
 
-      include ::Sidekiq::Worker if defined?(::Sidekiq)
+      if defined?(::Sidekiq)
 
-      def self.perform(*args)
-        new(*args).perform
-      end
-
-      def perform(*args)
-        resource = klass.is_a?(String) ? klass.constantize : klass
-        record = resource.find id
-        if tmp = record.send(:"#{column}_tmp")
-          asset = record.send(:"#{column}")
-          cache_dir  = [asset.root, asset.cache_dir].join("/")
-          cache_path = [cache_dir, tmp].join("/")
-          tmp_dir = [cache_dir, tmp.split("/")[0]].join("/")
-          record.send :"process_#{column}_upload=", true
-          record.send :"#{column}_tmp=", nil
-          File.open(cache_path) { |f| record.send :"#{column}=", f }
-          if record.save!
-            FileUtils.rm_r(tmp_dir, :force => true)
+        include ::Sidekiq::Worker
+        def perform(klass, id, column)
+          resource = klass.is_a?(String) ? klass.constantize : klass
+          record = resource.find id
+          if tmp = record.send(:"#{column}_tmp")
+            asset = record.send(:"#{column}")
+            cache_dir  = [asset.root, asset.cache_dir].join("/")
+            cache_path = [cache_dir, tmp].join("/")
+            tmp_dir = [cache_dir, tmp.split("/")[0]].join("/")
+            record.send :"process_#{column}_upload=", true
+            record.send :"#{column}_tmp=", nil
+            File.open(cache_path) { |f| record.send :"#{column}=", f }
+            if record.save!
+              FileUtils.rm_r(tmp_dir, :force => true)
+            end
           end
         end
+
+      else
+
+        @queue = :store_asset
+        def self.perform(*args)
+          new(*args).perform
+        end
+        def perform
+          resource = klass.is_a?(String) ? klass.constantize : klass
+          record = resource.find id
+          if tmp = record.send(:"#{column}_tmp")
+            asset = record.send(:"#{column}")
+            cache_dir  = [asset.root, asset.cache_dir].join("/")
+            cache_path = [cache_dir, tmp].join("/")
+            tmp_dir = [cache_dir, tmp.split("/")[0]].join("/")
+            record.send :"process_#{column}_upload=", true
+            record.send :"#{column}_tmp=", nil
+            File.open(cache_path) { |f| record.send :"#{column}=", f }
+            if record.save!
+              FileUtils.rm_r(tmp_dir, :force => true)
+            end
+          end
+        end
+
       end
       
     end # StoreAsset
