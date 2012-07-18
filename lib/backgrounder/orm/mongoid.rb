9ac2ae52fb0690cc216e5d3544c72330f10f50dd
Alex <alex@klepa.co>
2012-07-18 18:34:27 -0300

    Added Mongoid 3 support

--- lib/backgrounder/orm/mongoid.rb
+++ lib/backgrounder/orm/mongoid.rb
@@ -1,33 +1,35 @@
 module CarrierWave
   module Backgrounder
-    module ORM
+     module ORM   
 
-      module Mongoid
-        include CarrierWave::Backgrounder::ORM::Base
+        module Mongoid
+	      extend ActiveSupport::Concern        
+	      include CarrierWave::Backgrounder::ORM::Base
 
-        def process_in_background(column, worker=::CarrierWave::Workers::ProcessAsset)
-          super
+        module ClassMethods 
+          def process_in_background(column, worker=::CarrierWave::Workers::ProcessAsset)
+	          super
 
-          class_eval  <<-RUBY, __FILE__, __LINE__ + 1
-            def trigger_#{column}_background_processing?
-              process_#{column}_upload != true && #{column}_changed?
-            end
-          RUBY
-        end
+            class_eval  <<-RUBY, __FILE__, __LINE__ + 1
+		          def trigger_#{column}_background_processing?
+                process_#{column}_upload != true && #{column}_changed?
+		          end
+            RUBY
+          end
 
-        def store_in_background(column, worker=::CarrierWave::Workers::StoreAsset)
-          super
+          def store_in_background(column, worker=::CarrierWave::Workers::StoreAsset)
+	            super
 
-          class_eval  <<-RUBY, __FILE__, __LINE__ + 1
-            def trigger_#{column}_background_storage?
-              process_#{column}_upload != true && #{column}_changed?
-            end
-          RUBY
-        end
-      end # ActiveRecord
-
+              class_eval  <<-RUBY, __FILE__, __LINE__ + 1
+		            def trigger_#{column}_background_storage?
+                  process_#{column}_upload != true && #{column}_changed?
+		            end 
+              RUBY
+          end
+	      end # ClassMethods 
+      end # Mongoid
     end # ORM
-  end # Backgrounder
+  end # Backgrounder  
 end # CarrierWave
 
 Mongoid::Document::ClassMethods.send(:include, ::CarrierWave::Backgrounder::ORM::Mongoid)
