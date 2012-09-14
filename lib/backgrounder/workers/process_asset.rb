0d3be3483ac99d0141cc049fd1bd800205a6f704
Marc Beaupre <jack.quack@gmail.com>
2012-09-14 10:15:01 -0400

    Added all DJ callbacks to the process_asset job.

--- lib/backgrounder/workers/process_asset.rb
+++ lib/backgrounder/workers/process_asset.rb
@@ -28,6 +28,42 @@ module CarrierWave
         self.klass, self.id, self.column = klass, id, column
       end
 
+      def enqueue(job)
+        if @record.respond_to?(:enqueue_callback)
+          @record.enqueue_callback(job)
+        end
+      end
+
+      def before(job)
+        if @record.respond_to?(:before_callback)
+          @record.before_callback(job)
+        end
+      end
+
+      def after(job)
+        if @record.respond_to?(:after_callback)
+          @record.after_callback(job)
+        end
+      end
+
+      def success(job)
+        if @record.respond_to?(:success_callback)
+          @record.success_callback(job)
+        end
+      end
+
+      def error(job, exception)
+        if @record.respond_to?(:error_callback)
+          @record.error_callback(job, exception)
+        end
+      end
+
+      def failure
+        if @record.respond_to?(:failure_callback)
+          @record.failure_callback
+        end
+      end
+
     end # ProcessAsset
 
   end # Workers
