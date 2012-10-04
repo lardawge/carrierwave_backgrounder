844c155b0a3b2623bab58734f727693c02254bfd
Jay Zeschin <jay.zeschin@modeset.com>
2012-10-04 17:10:16 -0600

    Allow users to explicitly specify the queue backend to use
    * Add a configuration block for the main module
    * Make backend auto detection more specific
    * Refactor the job dispatching logic
    * Add some basic specs
    * Update the README with usage info
    * Rearrange autoload to the top of the main module
    * Suppress warning output in specs

--- lib/carrierwave_backgrounder.rb
+++ lib/carrierwave_backgrounder.rb
@@ -1,5 +1,6 @@
 module CarrierWave
   module Backgrounder
+
     Logger = Logger.new(STDOUT)
 
     autoload :Delay, 'backgrounder/delay'
@@ -8,6 +9,70 @@ module CarrierWave
     module ORM
       autoload :Base, 'backgrounder/orm/base'
     end
+
+    class << self
+      def backend=(value)
+        @backend = value
+        self.configure_backend
+      end
+
+      def backend
+        return @backend unless @backend.nil?
+        if available_backends.empty?
+          warn 'WARNING: No queue backends found to use for CarrierWave::Backgrounder'
+        elsif available_backends.size == 1
+          self.backend = available_backends.first
+        elsif available_backends.size > 1
+          warn 'WARNING: Multiple queue backends found for CarrierWave::Backgrounder. You need to set one explicitly.'
+        end
+      end
+
+      def configure
+        yield self
+      end
+
+      def available_backends
+        @available_backends ||= begin
+          backends = []
+          backends << :girl_friday if defined? ::GirlFriday
+          backends << :delayed_job if defined? ::Delayed::Job
+          backends << :resque      if defined? ::Resque
+          backends << :qu          if defined? ::Qu
+          backends << :sidekiq     if defined? ::Sidekiq
+          backends << :qc          if defined? ::QC
+          backends
+        end
+      end
+
+      def configure_backend
+        if backend == :girl_friday
+          require 'girl_friday'
+          @girl_friday_queue = GirlFriday::WorkQueue.new(:carrierwave) do |msg|
+            worker = msg[:worker]
+            worker.perform
+          end
+        end
+      end
+
+      def enqueue_for_backend(worker, class_name, subject_id, mounted_as)
+        case backend
+        when :girl_friday
+          @girl_friday_queue << { :worker => worker.new(self.class.name, subject_id, mounted_as) }
+        when :delayed_job
+          ::Delayed::Job.enqueue worker.new(class_name, subject_id, mounted_as)
+        when :resque
+          ::Resque.enqueue worker, class_name, subject_id, mounted_as
+        when :qu
+          ::Qu.enqueue worker, class_name, subject_id, column.mounted_as
+        when :sidekiq
+          ::Sidekiq::Client.enqueue worker, class_name, subject_id, mounted_as
+        when :qc
+          ::QC.enqueue "#{worker.name}.perform", class_name, subject_id, mounted_as.to_s
+        end
+      end
+
+    end
+
   end
 end
 
@@ -35,11 +100,3 @@ if defined?(Rails)
   end
 end
 
-if defined?(GirlFriday)
-  require 'girl_friday'
-
-  CARRIERWAVE_QUEUE = GirlFriday::WorkQueue.new(:carrierwave) do |msg|
-    worker = msg[:worker]
-    worker.perform
-  end
-end
---
+++ spec/support/backend_constants.rb
@@ -0,0 +1,23 @@
+# Fixture module declarations for backend detection testing
+
+module GirlFriday
+end
+
+module Delayed
+  module Job
+  end
+end
+
+module Resque
+end
+
+module Qu
+end
+
+module Sidekiq
+  module Worker
+  end
+end
+
+module QC
+end
