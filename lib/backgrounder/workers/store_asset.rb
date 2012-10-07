8390016d0b50da225dd9f380856d1b28fb789220
Marc Beaupre <jack.quack@gmail.com>
2012-10-06 20:11:45 -0400

    added the same callbacks to store_asset

--- lib/backgrounder/workers/store_asset.rb
+++ lib/backgrounder/workers/store_asset.rb
@@ -13,16 +13,17 @@ module CarrierWave
       def perform(*args)
         set_args(*args) unless args.empty?
         resource = klass.is_a?(String) ? klass.constantize : klass
-        record = resource.find id
-        if tmp = record.send(:"#{column}_tmp")
-          asset = record.send(:"#{column}")
+        @record = resource.find id
+        
+        if tmp = @record.send(:"#{column}_tmp")
+          asset = @record.send(:"#{column}")
           cache_dir  = [asset.root, asset.cache_dir].join("/")
           cache_path = [cache_dir, tmp].join("/")
           tmp_dir = [cache_dir, tmp.split("/")[0]].join("/")
-          record.send :"process_#{column}_upload=", true
-          record.send :"#{column}_tmp=", nil
-          File.open(cache_path) { |f| record.send :"#{column}=", f }
-          if record.save!
+          @record.send :"process_#{column}_upload=", true
+          @record.send :"#{column}_tmp=", nil
+          File.open(cache_path) { |f| @record.send :"#{column}=", f }
+          if @record.save!
             FileUtils.rm_r(tmp_dir, :force => true)
           end
         end
@@ -30,6 +31,42 @@ module CarrierWave
       
       def set_args(klass, id, column)
         self.klass, self.id, self.column = klass, id, column
+      end
+
+      def enqueue(job)
+        if @record.respond_to?(:enqueue_callback)
+          @record.enqueue_callback(job)
+        end
+      end
+
+      def before(job)
+        if @record.respond_to?(:before_callback)
+          @record.before_callback(job)
+        end
+      end
+
+      def after(job)
+        if @record.respond_to?(:after_callback)
+          @record.after_callback(job)
+        end
+      end
+
+      def success(job)
+        if @record.respond_to?(:success_callback)
+          @record.success_callback(job)
+        end
+      end
+
+      def error(job, exception)
+        if @record.respond_to?(:error_callback)
+          @record.error_callback(job, exception)
+        end
+      end
+
+      def failure
+        if @record.respond_to?(:failure_callback)
+          @record.failure_callback
+        end
       end
 
     end # StoreAsset
