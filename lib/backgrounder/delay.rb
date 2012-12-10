02993b7e1e0427b48f0706ae658c0feff04164f5
Leszek Zalewski <leszek@nextsocial.de>
2012-12-10 17:41:13 +0100

    Override store_versions! when using backgrounder
    Carrierwave is trying to store versions from cache, when you pass
    `*_cache` via form, so we need to check if we are processing it or not.

--- lib/backgrounder/delay.rb
+++ lib/backgrounder/delay.rb
@@ -7,7 +7,7 @@ module CarrierWave
         Logger.warn "Carrierwave::Backgrounder::DelayStorage is deprecated, use Carrierwave::Backgrounder::Delay instead"
       end
     end
-    
+
     module Delay
 
       ##
@@ -16,16 +16,20 @@ module CarrierWave
         super(new_file) if proceed_with_versioning?
       end
 
+      def store_versions!(*args)
+        super(*args) if proceed_with_versioning?
+      end
+
       def process!(new_file=nil)
         super(new_file) if proceed_with_versioning?
       end
-      
+
       private
 
       def proceed_with_versioning?
         !model.respond_to?(:"process_#{mounted_as}_upload") || model.send(:"process_#{mounted_as}_upload")
       end
     end # Delay
-    
+
   end # Backgrounder
 end # CarrierWave
