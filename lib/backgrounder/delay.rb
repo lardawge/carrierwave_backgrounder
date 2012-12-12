21f00cee372e23d0a708a8b0ea4088b2fec76598
Larry Sprock <larry@lucidbleu.com>
2012-12-12 15:06:16 -0800

    No reason to pass args to super since that is the default

--- lib/backgrounder/delay.rb
+++ lib/backgrounder/delay.rb
@@ -3,19 +3,19 @@ module CarrierWave
 
     module Delay
       def cache_versions!(new_file)
-        super(new_file) if proceed_with_versioning?
+        super if proceed_with_versioning?
       end
 
       def process!(new_file=nil)
-        super(new_file) if proceed_with_versioning?
+        super if proceed_with_versioning?
       end
-      
+
       private
 
       def proceed_with_versioning?
         !model.respond_to?(:"process_#{mounted_as}_upload") || model.send(:"process_#{mounted_as}_upload")
       end
     end # Delay
-    
+
   end # Backgrounder
 end # CarrierWave
