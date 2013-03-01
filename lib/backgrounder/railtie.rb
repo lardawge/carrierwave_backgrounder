dd7eb9112f709afacec57f7be3054d63ab4b65ff
Larry Sprock <larry@lucidbleu.com>
2013-02-28 18:55:43 -0800

    Move railtie into backgrounder/railtie

---
+++ lib/backgrounder/railtie.rb
@@ -0,0 +1,25 @@
+module CarrierWave
+  module Backgrounder
+    class Railtie < Rails::Railtie
+
+      initializer "carrierwave_backgrounder.active_record" do
+        ActiveSupport.on_load :active_record do
+          require 'backgrounder/orm/activemodel'
+          ::ActiveRecord::Base.extend CarrierWave::Backgrounder::ORM::ActiveModel
+        end
+      end
+
+      initializer "carrierwave_backgrounder.data_mapper", :before =>"data_mapper.add_to_prepare" do
+        require 'backgrounder/orm/data_mapper' if defined?(DataMapper)
+      end
+
+      initializer "carrierwave_backgrounder.mongoid" do
+        if defined?(Mongoid)
+          require 'backgrounder/orm/activemodel'
+          ::Mongoid::Document::ClassMethods.send(:include, ::CarrierWave::Backgrounder::ORM::ActiveModel)
+        end
+      end
+
+    end
+  end
+end
--- lib/carrierwave_backgrounder.rb
+++ lib/carrierwave_backgrounder.rb
@@ -27,30 +27,4 @@ module CarrierWave
   end
 end
 
-if defined?(Rails)
-  module CarrierWave
-    module Backgrounder
-      class Railtie < Rails::Railtie
-
-        initializer "carrierwave_backgrounder.active_record" do
-          ActiveSupport.on_load :active_record do
-            require 'backgrounder/orm/activemodel'
-            ::ActiveRecord::Base.extend CarrierWave::Backgrounder::ORM::ActiveModel
-          end
-        end
-
-        initializer "carrierwave_backgrounder.data_mapper", :before =>"data_mapper.add_to_prepare" do
-          require 'backgrounder/orm/data_mapper' if defined?(DataMapper)
-        end
-
-        initializer "carrierwave_backgrounder.mongoid" do
-          if defined?(Mongoid)
-            require 'backgrounder/orm/activemodel'
-            ::Mongoid::Document::ClassMethods.send(:include, ::CarrierWave::Backgrounder::ORM::ActiveModel)
-          end
-        end
-
-      end
-    end
-  end
-end
+require 'backgrounder/railtie' if defined?(Rails)
