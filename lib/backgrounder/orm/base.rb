33bb9176caef491c3f9e6295b6a4edab88ca535e
Mal Curtis <mal@sitepoint.com>
2013-03-16 08:58:51 +1300

    Don't process unsaved records

--- lib/backgrounder/orm/base.rb
+++ lib/backgrounder/orm/base.rb
@@ -100,7 +100,7 @@ module CarrierWave
             def #{column}_updated?; true; end
 
             def enqueue_#{column}_background_job?
-              !remove_#{column}? && !process_#{column}_upload && #{column}_updated?
+              !remove_#{column}? && !process_#{column}_upload && #{column}_updated? && !new_record?
             end
 
             def enqueue_#{column}_background_job
