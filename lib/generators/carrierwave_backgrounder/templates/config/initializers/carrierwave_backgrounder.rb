0873cde0fc9f904a35ce5b21987c45816a40466e
Espen Antonsen <espen@inspired.no>
2013-04-19 02:09:57 +0300

    added template for sucker_punch in generator

--- lib/generators/carrierwave_backgrounder/templates/config/initializers/carrierwave_backgrounder.rb
+++ lib/generators/carrierwave_backgrounder/templates/config/initializers/carrierwave_backgrounder.rb
@@ -3,6 +3,7 @@ CarrierWave::Backgrounder.configure do |c|
   # c.backend :resque, queue: :carrierwave
   # c.backend :sidekiq, queue: :carrierwave
   # c.backend :girl_friday, queue: :carrierwave
+  # c.backend :sucker_punch, queue: :carrierwave
   # c.backend :qu, queue: :carrierwave
   # c.backend :qc
 end
