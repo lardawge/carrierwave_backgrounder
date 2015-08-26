fe8b3935f3ee4fcab753255e748fefb7ae2bd9ae
Larry Sprock <larry@lucidbleu.com>
2015-08-26 12:09:05 -0700

    [FIX] Bug where nothing was backgrounded

--- lib/backgrounder/delay.rb
+++ lib/backgrounder/delay.rb
@@ -17,9 +17,8 @@ module CarrierWave
       private
 
       def proceed_with_versioning?
-        !model.respond_to?(:"process_#{mounted_as}_upload") ||
-            model.send(:"process_#{mounted_as}_upload") ||
-            enable_processing
+        !model.respond_to?(:"process_#{mounted_as}_upload") && enable_processing ||
+          !!(model.send(:"process_#{mounted_as}_upload") && enable_processing)
       end
     end # Delay
 
