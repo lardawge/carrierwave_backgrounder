bcb41af3a9d05903f5728b98408edbec17911725
debbbbie <debbbbie@163.com>
2015-10-23 10:13:43 +0800

    Update #{column}_processed if has this column
    Should update `#{column}_processed`, because it is often used.

--- lib/backgrounder/workers/process_asset.rb
+++ lib/backgrounder/workers/process_asset.rb
@@ -12,6 +12,9 @@ module CarrierWave
           if record.send(:"#{column}").recreate_versions! && record.respond_to?(:"#{column}_processing")
             record.update_attribute :"#{column}_processing", false
           end
+          if record.respond_to?(:"#{column}_processed")
+            record.update_attribute :"#{column}_processed", true
+          end          
         end
       end
 
