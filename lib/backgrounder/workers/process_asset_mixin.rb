9e4717034027aaea6ffcca1d2c89ec4649f9d6b1
Yaro <vergilet@users.noreply.github.com>
2022-03-18 13:37:06 +0200

    Update process_asset_mixin.rb

--- lib/backgrounder/workers/process_asset_mixin.rb
+++ lib/backgrounder/workers/process_asset_mixin.rb
@@ -15,9 +15,13 @@ module CarrierWave
         if record && record.send(:"#{column}").present?
           record.send(:"process_#{column}_upload=", true)
           if record.send(:"#{column}").recreate_versions! && record.respond_to?(:"#{column}_processing")
+            logger.debug { "good" }
+            logger.debug { "My args: #{args.inspect}" }
             record.update_attribute :"#{column}_processing", false
           end
         else
+          logger.debug { "notready" }
+          logger.debug { "My args: #{args.inspect}" }
           when_not_ready
         end
       end
