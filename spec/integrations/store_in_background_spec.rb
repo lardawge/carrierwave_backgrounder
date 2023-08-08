2fe006c849b39a19b19ded95f7474ee69074405f
Larry Sprock <larry@lucidbleu.com>
2023-08-08 11:34:17 -0700

    Add specs for store_in_background

--- spec/integrations/store_in_background_spec.rb
+++ spec/integrations/store_in_background_spec.rb
@@ -3,18 +3,32 @@ require 'rails_helper'
 RSpec.describe '::store_in_background', clear_images: true do
   let(:user) { User.new }
 
-  before(:each) do
-    expect(file_count('spec/support/dummy_app/tmp/images')).to eql(0)
-    user.avatar = load_file('spec/support/fixtures/images/test-1.jpg')
-    user.save
+  context 'when assigning an asset' do
+    before(:each) do
+      expect(file_count('spec/support/dummy_app/tmp/images')).to eql(0)
+      user.avatar = load_file('spec/support/fixtures/images/test-1.jpg')
+      user.save
+    end
+
+    it 'creates a temp file and stores the path' do
+      expect(file_count('spec/support/dummy_app/tmp/images')).to eql(1)
+      expect(user.avatar_tmp).to include('test-1.jpg')
+    end
+
+    it 'creates a background job in carrierwave queue' do
+      expect(Sidekiq::Queues["carrierwave"].size).to eql(1)
+    end
   end
 
-  it 'creates a temp file and stores the path' do
-    expect(file_count('spec/support/dummy_app/tmp/images')).to eql(1)
-    expect(user.avatar_tmp).to include('test-1.jpg')
-  end
+  context 'when saving a record' do
+    let!(:user) {
+      Sidekiq::Testing.inline! do
+        User.create(avatar: load_file('spec/support/fixtures/images/test-1.jpg'))
+      end
+    }
 
-  it 'creates a background job in queue' do
-
+    it 'does not enqueue a new job' do
+      expect { user.reload.save }.to_not change(Sidekiq::Queues["carrierwave"], :size)
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require_relative 'support/dummy_app/config/environment'
 require_relative 'support/global_macros'
-
+require 'sidekiq/testing'
 require 'rspec/rails'
 require 'backgrounder/railtie'
 
@@ -21,7 +21,9 @@ RSpec.configure do |config|
   end
 
   config.after(:example, clear_images: true) do
+    Sidekiq::Queues.clear_all
     FileUtils.rm_rf Dir.glob("spec/support/dummy_app/tmp/images/*")
+    FileUtils.rm_rf Dir.glob("spec/support/dummy_app/public/uploads/*")
   end
 end
 
