f80826c47c6f0de02528cfcea55d07ac481da7af
Larry Sprock <larry@lucidbleu.com>
2023-08-08 19:54:03 -0700

    Remove options for other queues from config

--- lib/generators/carrierwave_backgrounder/templates/config/initializers/carrierwave_backgrounder.rb
+++ lib/generators/carrierwave_backgrounder/templates/config/initializers/carrierwave_backgrounder.rb
@@ -1,9 +1,4 @@
 CarrierWave::Backgrounder.configure do |c|
   c.backend :active_job, queue: :carrierwave
   # c.backend :sidekiq, queue: :carrierwave
-  # c.backend :delayed_job, queue: :carrierwave
-  # c.backend :resque, queue: :carrierwave
-  # c.backend :sucker_punch, queue: :carrierwave
-  # c.backend :qu, queue: :carrierwave
-  # c.backend :qc
 end
