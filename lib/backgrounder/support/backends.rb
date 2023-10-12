ccdf43359cd339839121bf532f0fbc4f605ba604
Adam Migodzinski <adam.migodzinski@goodylabs.com>
2023-10-12 22:00:23 +0200

    bring back dj

--- lib/backgrounder/support/backends.rb
+++ lib/backgrounder/support/backends.rb
@@ -32,6 +32,21 @@ module CarrierWave
             worker.client_push(args)
           end
 
+          def enqueue_delayed_job(worker, *args)
+            worker_args = {}
+            if ::Delayed::Job.new.respond_to?(:queue)
+              worker_args[:queue] = queue_options[:queue] if queue_options[:queue]
+              worker_args[:priority] = queue_options[:priority] if queue_options[:priority]
+              ::Delayed::Job.enqueue worker.new(*args), worker_args
+            else
+              worker_args[:priority] = queue_options[:priority] if queue_options[:priority]
+              ::Delayed::Job.enqueue worker.new(*args), worker_args
+              if queue_options[:queue]
+                ::Rails.logger.warn("Queue name given but no queue column exists for Delayed::Job")
+              end
+            end
+          end
+
           private
 
           def sidekiq_queue_options(override_queue_name, args)
